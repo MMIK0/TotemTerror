edabc8fecb4f1de31493ce654b6f51b26b874a7e
MMIK0 <35567342+MMIK0@users.noreply.github.com>
2021-12-09 15:42:41 +0200

    Added a spike trap
    An animation to spiketrap, and spites for them.

---
+++ Assets/TrapSetup.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrapSetup : MonoBehaviour
+{
+    public Animator[] animators;
+
+    public void OnTriggerEnter(Collider collision)
+    {
+        foreach(Animator anim in animators)
+        {
+            anim.SetTrigger("Step");
+        }
+    }
+    public void OnTriggerExit(Collider other)
+    {
+        foreach (Animator anim in animators)
+        {
+            anim.ResetTrigger("Step");
+        }
+    }
+}
