7151d3d2f6b655f54507d94a7cca0e4f3d6c6d3b
Rastenbe <rasmus.criticalcharm@gmail.com>
2021-12-13 02:19:45 +0200

    BossImplemented polayer health added

---
+++ Assets/Scripts/PlayerHealthBar.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealthBar : MonoBehaviour
+{
+    public static PlayerHealthBar instance;
+    public Transform bar;
+
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+        DontDestroyOnLoad(gameObject);
+    }
+    public void UpdateHealthbar(float currentHp,float maxHp)
+    {
+        if (!bar)
+            return;
+        bar.localScale = new Vector3(1,currentHp / maxHp, 1);
+    }
+}
