0a14beb33806e04f2e898f4c462538e20eeb6832
MMIK0 <35567342+MMIK0@users.noreply.github.com>
2021-12-16 16:40:37 +0200

    Created a pausemenu and added it into scenes
    Also start level does not contain spawntrigger atm cause the new checkpoint system, will fix laterrr

---
+++ Assets/PauseMenu.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject panel;
+    bool paused = false;
+
+    private void Update()
+    {
+        Debug.Log(paused);
+        if (Input.GetKeyDown(KeyCode.Escape) && paused == false)
+        {
+            panel.SetActive(true);
+            paused = true;
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+        else if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            panel.SetActive(false);
+            Cursor.visible = false;
+            Cursor.lockState = CursorLockMode.Locked;
+            paused = false;
+        }
+    }
+}
